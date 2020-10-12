22a12f6044cea1d1da34d5ad2d53f571934c250c
AlienDev66 <p.capitango65@gmail.com>
2020-10-12 23:00:11 +0100

    :sparkles: Criando componente de Landing Page.

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+import {
+  ContentWrapper,
+  ImageLogo,
+  Location,
+  Main,
+  PageLanding,
+} from "./styles";
+import logoImg from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+
+const Landing: React.FC = () => {
+  return (
+    <PageLanding>
+      <ContentWrapper>
+        <ImageLogo src={logoImg} alt="Happy" />
+
+        <Main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </Main>
+
+        <Location>
+          <strong>Luanda</strong>
+          <span>Cazenga</span>
+        </Location>
+
+        <Link to="/orphanage" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
+        </Link>
+      </ContentWrapper>
+    </PageLanding>
+  );
+};
+
+export default Landing;
