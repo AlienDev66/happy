87e50174aba15e2d442f1faaf74a0f2cd2ab94da
AlienDev66 <p.capitango65@gmail.com>
2020-10-12 23:00:47 +0100

    :sparkles: Criando componentes de OrphanageMaps.

---
+++ web/src/pages/OrphanageMaps/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const OrphanageMaps: React.FC = () => {
+  return (
+    <Container>
+      <h1>OrphanageMaps</h1>
+    </Container>
+  );
+};
+
+export default OrphanageMaps;
