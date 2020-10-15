2b7abd66e1ba005f467bff5675b084a6c70e1d6c
AlienDev66 <p.capitango65@gmail.com>
2020-10-15 01:23:23 +0100

    :sparkles: Terminando o frontend e consumindo dados da api.

--- web/src/pages/OrphanageMaps/styles.ts
+++ web/src/pages/OrphanageMaps/styles.ts
@@ -1,77 +1,102 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const PageMap = styled.div`
-width: 100vw;
-height: 100vh;
+  width: 100vw;
+  height: 100vh;
 
-position: relative;
-display: flex;
+  position: relative;
+  display: flex;
 
-aside{
- width: 440px;
- background: linear-gradient(329.54deg, #29B6D1 0%, #00C7C7 100%);
+  aside {
+    width: 440px;
+    background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
 
- padding: 80px;
- display: flex;
- flex-direction: column;
- justify-content: space-between;
-}
+    padding: 80px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
-aside h2{
- font-size: 40px;
- font-weight: 800;
- line-height: 42px;
- margin-top: 64px;
-}
+  aside h2 {
+    font-size: 40px;
+    font-weight: 800;
+    line-height: 42px;
+    margin-top: 64px;
+  }
 
-aside p{
- line-height: 28px;
- margin-top: 24px;
-}
+  aside p {
+    line-height: 28px;
+    margin-top: 24px;
+  }
 
-aside footer{
- display: flex;
- flex-direction: column;
+  aside footer {
+    display: flex;
+    flex-direction: column;
 
- line-height: 24px;
-}
+    line-height: 24px;
+  }
 
-aside footer strong{
- font-weight: 800;
-}
+  aside footer strong {
+    font-weight: 800;
+  }
 
-.leaflet-container{
- z-index: 5;
-}
+  .leaflet-container {
+    z-index: 5;
+  }
 
-.create-orphanage{
- position: absolute;
+  .create-orphanage {
+    position: absolute;
 
- right: 40px;
- bottom: 40px;
+    right: 40px;
+    bottom: 40px;
 
- height: 64px;
- width: 64px;
- background: #15c3d6;
+    height: 64px;
+    width: 64px;
+    background: #15c3d6;
 
-border-radius: 20px;
+    border-radius: 20px;
 
-display: flex;
-justify-content: center;
-align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-z-index: 10;
+    z-index: 10;
 
-transition: background-color 0.2s;
+    transition: background-color 0.2s;
 
-&:hover{
- background: #17d6eb;
-}
+    &:hover {
+      background: #17d6eb;
+    }
+  }
+  .map-popup .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+  .map-popup .leaflet-popup-content {
+    color: #0089a5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .map-popup .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    background: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+    border-radius: 12px;
 
-}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
+  .map-popup .leaflet-popup-tip-container {
+    display: none;
+  }
 `;
 
-export const MapMarkerImg = styled.img`
-
-`;
+export const MapMarkerImg = styled.img``;
