c899202c74bca596e2278ce3a992a0003f83e9e8
AlienDev66 <p.capitango65@gmail.com>
2020-10-15 01:22:56 +0100

    :sparkles: Terminando o frontend e consumindo dados da api.

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
+import CreateOrphanage from "./pages/CreateOrphanage";
 import Landing from "./pages/Landing";
+import OrphanageList from "./pages/OrphanageList";
 import OrphanageMaps from "./pages/OrphanageMaps";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route component={Landing} path="/" exact />
-        <Route component={OrphanageMaps} path="/orphanage" />
+        <Route path="/" component={Landing} exact />
+        <Route path="/app" component={OrphanageMaps} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={OrphanageList} />
       </Switch>
     </BrowserRouter>
   );
